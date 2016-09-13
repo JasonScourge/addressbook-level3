a950a3353658c2605de915e61f44cc03f978b9df
Acer\Jason Scourge <jasonscourge@gmail.com>
2016-09-14 02:28:21 +0800

    Commit errors might not be reflected; apologise for any inconvience caused

--- src/seedu/addressbook/ui/Gui.java
+++ src/seedu/addressbook/ui/Gui.java
@@ -15,37 +15,37 @@ import java.io.IOException;
  */
 public class Gui {
 
-    /** Offset required to convert between 1-indexing and 0-indexing.  */
-    public static final int DISPLAYED_INDEX_OFFSET = 1;
+	/** Offset required to convert between 1-indexing and 0-indexing.  */
+	public static final int DISPLAYED_INDEX_OFFSET = 1;
 
-    public static final int INITIAL_WINDOW_WIDTH = 800;
-    public static final int INITIAL_WINDOW_HEIGHT = 600;
-    private final Logic logic;
+	public static final int INITIAL_WINDOW_WIDTH = 800;
+	public static final int INITIAL_WINDOW_HEIGHT = 600;
+	private final Logic logic;
 
-    private MainWindow mainWindow;
-    private String version;
+	private MainWindow mainWindow;
+	private String version;
 
-    public Gui(Logic logic, String version) {
-        this.logic = logic;
-        this.version = version;
-    }
+	public Gui(Logic logic, String version) {
+		this.logic = logic;
+		this.version = version;
+	}
 
-    public void start(Stage stage, Stoppable mainApp) throws IOException {
-        mainWindow = createMainWindow(stage, mainApp);
-        mainWindow.displayWelcomeMessage(version, logic.getStorageFilePath());
-    }
+	public void start(Stage stage, Stoppable mainApp) throws IOException {
+		mainWindow = createMainWindow(stage, mainApp);
+		mainWindow.displayWelcomeMessage(version, logic.getStorageFilePath());
+	}
 
-    private MainWindow createMainWindow(Stage stage, Stoppable mainApp) throws IOException{
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("ui" + File.separator + "mainwindow.fxml"));
-        stage.setTitle(version);
-        stage.setScene(new Scene(loader.load(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT));
-        stage.getIcons().add(new Image("file:src/seedu/addressbook/storage/Callidus.jpg"));
-        stage.show();
-        MainWindow mainWindow = loader.getController();
-        mainWindow.setLogic(logic);
-        mainWindow.setMainApp(mainApp);
-        return mainWindow;
-    }
+	private MainWindow createMainWindow(Stage stage, Stoppable mainApp) throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("ui" + File.separator + "mainwindow.fxml"));
+		stage.setTitle(version);
+		stage.setScene(new Scene(loader.load(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT));
+		stage.getIcons().add(new Image("file:src/seedu/addressbook/storage/lolcron.jpg"));
+		stage.show();
+		MainWindow mainWindow = loader.getController();
+		mainWindow.setLogic(logic);
+		mainWindow.setMainApp(mainApp);
+		return mainWindow;
+	}
 
 }
