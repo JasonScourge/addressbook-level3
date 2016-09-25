79df37cb0ceb3f81f61c237d6b10e726f50d29d1
Acer\Jason Scourge <jasonscourge@gmail.com>
2016-09-25 21:01:41 +0800

    Logic and StorageFile now depend on the abstraction Storage

--- src/seedu/addressbook/logic/Logic.java
+++ src/seedu/addressbook/logic/Logic.java
@@ -2,10 +2,11 @@ package seedu.addressbook.logic;
 
 import seedu.addressbook.commands.Command;
 import seedu.addressbook.commands.CommandResult;
+import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.parser.Parser;
-import seedu.addressbook.storage.StorageFile;
+import seedu.addressbook.storage.Storage;
 
 import java.util.Collections;
 import java.util.List;
@@ -15,9 +16,7 @@ import java.util.Optional;
  * Represents the main Logic of the AddressBook.
  */
 public class Logic {
-
-
-    private StorageFile storage;
+    private Storage storage;
     private AddressBook addressBook;
 
     /** The list of person shown to the user most recently.  */
@@ -28,12 +27,12 @@ public class Logic {
         setAddressBook(storage.load());
     }
 
-    Logic(StorageFile storageFile, AddressBook addressBook){
+    Logic(Storage storageFile, AddressBook addressBook){
         setStorage(storageFile);
         setAddressBook(addressBook);
     }
 
-    void setStorage(StorageFile storage){
+    void setStorage(Storage storage){
         this.storage = storage;
     }
 
@@ -43,10 +42,10 @@ public class Logic {
 
     /**
      * Creates the StorageFile object based on the user specified path (if any) or the default storage path.
-     * @throws StorageFile.InvalidStorageFilePathException if the target file path is incorrect.
+     * @throws IllegalValueException if the target file path is incorrect.
      */
-    private StorageFile initializeStorage() throws StorageFile.InvalidStorageFilePathException {
-        return new StorageFile();
+    private Storage initializeStorage() throws IllegalValueException{
+        return new Storage();
     }
 
     public String getStorageFilePath() {
---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,59 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.storage.Storage.StorageOperationException;
+
+/*
+ *  Represents the class used to store address book data. 
+ */
+public class Storage {
+    /**
+     *  Constructor
+     */
+    public Storage(){
+        // Does Nothing
+    }
+
+    /**
+     * Signals that the given file path does not fulfill the storage filepath constraints.
+     */
+    public static class InvalidStorageFilePathException extends IllegalValueException {
+        public InvalidStorageFilePathException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Signals that some error has occured while trying to convert and read/write data between the application
+     * and the storage file.
+     */
+    public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
+        }
+    }
+    
+    /**
+     * Saves all data to this storage file.
+     *
+     * @throws StorageOperationException if there were errors converting and/or storing data to file.
+     */
+    public void save(AddressBook addressBook) throws StorageOperationException {
+        // Do not need to return anything
+    }
+    
+    /**
+     * Loads data from this storage file.
+     *
+     * @throws StorageOperationException if there were errors reading and/or converting data from file.
+     */
+    public AddressBook load() throws StorageOperationException {
+        return null;
+    }
+
+    public String getPath(){
+        return " ";
+    }
+
+}
